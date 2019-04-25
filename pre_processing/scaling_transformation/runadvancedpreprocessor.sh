11bcdcd48fa1c7515e6e0902242e6cac02352e4b
abarbulescu <alexandru@appticles.com>
2019-04-25 14:07:34 +0300

    Remove useless jars

--- pre_processing/scaling_transformation/runadvancedpreprocessor.sh
+++ pre_processing/scaling_transformation/runadvancedpreprocessor.sh
@@ -1,5 +1,5 @@
 cp -r /opt/ga_chp_bq /opt/code
 cd /opt/code
 git pull
-spark-submit --jars /opt/spark/jars/spark-cassandra-connector.jar,/opt/spark/jars/jsr166e.jar,/opt/spark/jars/spark-avro.jar /opt/code/pre_processing/scaling_transformation/ga_chp_bq_advanced_preprocessor.py
+spark-submit --jars /opt/spark/jars/jsr166e.jar /opt/code/pre_processing/scaling_transformation/ga_chp_bq_advanced_preprocessor.py
 
--- training/model_generator/runmodelgenerator.sh
+++ training/model_generator/runmodelgenerator.sh
@@ -1,5 +1,5 @@
 cp -r /opt/ga_chp_bq /opt/code
 cd /opt/code
 git pull
-spark-submit --jars /opt/spark/jars/spark-cassandra-connector.jar,/opt/spark/jars/jsr166e.jar,/opt/spark/jars/spark-avro.jar /opt/code/training/model_generator/ga_chp_bq_model_generator.py
+spark-submit --jars /opt/spark/jars/jsr166e.jar /opt/code/training/model_generator/ga_chp_bq_model_generator.py
 
